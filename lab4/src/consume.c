7c8a622696b7917b05855e780a4fdc5056343274
Akshay Budhkar <akshaybudhkar@gmail.com>
2014-11-05 22:46:51 -0500

    Comments and formats the code

--- lab4/src/consume.c
+++ lab4/src/consume.c
@@ -7,38 +7,42 @@
 #include <time.h>
 
 mqd_t qdes;
-sem_t * sem_c;
+sem_t * sem_c; // named semaphore for the consumer
 
 int consume(int expect) {
-	//while (sem_wait(sem_c) == 0) {
 	while (expect--) {
 		int item;
+        // try to remove the oldest item in the queue
 		if (mq_receive(qdes, (char *)&item, sizeof(int), NULL) == -1) {
 			perror("mq_receive() failed");
 		}
 		printf("Consumed integer %d\n", item);
 	}
-
+    
 	return 0;
 }
 
 int main(int argc, char *argv[]) {
-        char *qname = "/a24joshi_mqueue";
-        qdes  = mq_open(qname, O_RDWR);
-
-        if (qdes == -1 ) {
-                perror("mq_open() failed");
-                exit(1);
-        }
-
+    char *qname = "/a24joshi_mqueue";
+    qdes  = mq_open(qname, O_RDWR); // open the queue with the given queue name
+    
+    // check if opening the queue was successful
+    if (qdes == -1 ) {
+        perror("mq_open() failed");
+        exit(1);
+    }
+    
+    // initialize the consumer semaphore with 0 as the initial value
 	sem_c = sem_open("/semc_a24joshi", 0);
-
+    
+    // consume the items added into the queue by the producer
 	consume(atoi(argv[1]));
-
-        if (mq_close(qdes) == -1) {
-                perror("mq_close() failed");
-                exit(2);
-        }
-
+    
+    // try to close the queue
+    if (mq_close(qdes) == -1) {
+        perror("mq_close() failed");
+        exit(2);
+    }
+    
 	return 0;
 }
--- lab4/src/produce.c
+++ lab4/src/produce.c
@@ -11,26 +11,28 @@
 #include <time.h>
 
 mqd_t qdes;
-sem_t sem_p;
-sem_t * sem_c;
+sem_t sem_p; // unnamed semaphore for the producer
+sem_t * sem_c; //named semaphore for the consumer
 
 int produce(int expect) {
 	// Seed the RNG with a random-ish value.
-        srand(time(0));
-
-        while (expect--) {
-        //while (sem_trywait(&sem_p) == 0) {
+    srand(time(0));
+    
+    while (expect--) {
 		int random = rand();
-                if (mq_send(qdes, (char *)&random, sizeof(int), 0) == -1) {
-                        perror("mq_send() failed");
-                }
-                printf("Produced integer %d\n", random);
+        // try to add a random integer to the queue
+        if (mq_send(qdes, (char *)&random, sizeof(int), 0) == -1) {
+            perror("mq_send() failed");
+        }
+        printf("Produced integer %d\n", random);
+        // increment the consumer semaphore
 		sem_post(sem_c);
 	}
-
+    
 	return 0;
 }
 
+// Calculates the initialization and the setup time for the process
 double computeTime(struct timeval *bench, struct timeval *ref) {
 	int secDiff = bench->tv_sec - ref->tv_sec;
 	int usecDiff = bench->tv_usec - ref->tv_usec;
@@ -41,55 +43,73 @@ int main(int argc, char *argv[]) {
 	struct timeval start, setup, complete;
 	struct timezone tz;
 	gettimeofday(&start, &tz);
-
-        char *qname = "/a24joshi_mqueue";
+    
+    char *qname = "/a24joshi_mqueue";
 	int oflag = O_RDWR | O_CREAT;
-        mode_t mode = S_IRUSR | S_IWUSR;
-        struct mq_attr attr;
-
-        if ( argc !=3 ) {
-                printf("Usage: %s <N> <B>\n", argv[0]);
-                exit(1);
-        }
-
-        attr.mq_maxmsg  = atoi(argv[2]);
-        attr.mq_msgsize = sizeof(int);
-        attr.mq_flags   = 0;            /* a blocking queue  */
-
-        qdes  = mq_open(qname, oflag, mode, &attr);
-        if (qdes == -1 ) {
-                perror("mq_open() failed");
-                exit(1);
-        }
-
+    mode_t mode = S_IRUSR | S_IWUSR;
+    struct mq_attr attr;
+    
+    // check for valid number of arguments
+    if ( argc !=3 ) {
+        printf("Usage: %s <N> <B>\n", argv[0]);
+        exit(1);
+    }
+    
+    // Set the queue attributes
+    attr.mq_maxmsg  = atoi(argv[2]);
+    attr.mq_msgsize = sizeof(int);
+    attr.mq_flags   = 0;            /* a blocking queue  */
+    
+    // create the queue with the given queue name and attributes
+    qdes  = mq_open(qname, oflag, mode, &attr);
+    
+    // check if creating the queue was successful
+    if (qdes == -1 ) {
+        perror("mq_open() failed");
+        exit(1);
+    }
+    
+    // initialize the unnamed producer semaphore
 	sem_init(&sem_p, 0, atoi(argv[1]));
+    
+    // create the named consumer semaphore
 	sem_c = sem_open("/semc_a24joshi", O_CREAT, mode, 0);
-
+    
+    // create a child process using fork
 	pid_t child_pid = fork();
+    
+    // if the process is a child, then replace the current process image with a new one
 	if (child_pid == 0) {
 		execl("./consume", "consume", argv[1], (char *)NULL);
 	}
+    
 	gettimeofday(&setup, &tz);
-
+    
+    // produce the number of random items asked by the user
 	produce(atoi(argv[1]));
-
-        if (mq_close(qdes) == -1) {
-                perror("mq_close() failed");
-                exit(2);
-        }
+    
+    // try to close the queue
+    if (mq_close(qdes) == -1) {
+        perror("mq_close() failed");
+        exit(2);
+    }
 	
+    // wait for a state change in the child process
 	int status;
 	wait(&status);
-
-        if (mq_unlink(qname) != 0) {
-                perror("mq_unlink() failed");
-                exit(3);
-        }
+    
+    // try to remove the queu
+    if (mq_unlink(qname) != 0) {
+        perror("mq_unlink() failed");
+        exit(3);
+    }
+    
 	gettimeofday(&complete, &tz);
-
+    
+    // print the initialization and data transmission times
 	printf("Time to initialize system: %f seconds\n", computeTime(&setup, &start));
 	printf("Time to transmit data: %f seconds\n", computeTime(&complete, &start));
-
+    
 	return 0;
 }
 
