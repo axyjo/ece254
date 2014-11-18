eaa6b62cf824665ed1c02ee975277820a4987f9a
Akshay Budhkar <akshaybudhkar@gmail.com>
2014-11-18 17:27:57 -0500

    Fixes code identation for inter-process communication program

--- lab5/processes/consume.c
+++ lab5/processes/consume.c
@@ -31,39 +31,39 @@ int consume(int id) {
         // increase the producer semaphore
         sem_post(mayProduce);
     }
-
+    
     return 0;
 }
 
 int main(int argc, char *argv[]) {
     char *qname = "/a24joshi_mqueue";
     qdes  = mq_open(qname, O_RDWR); // open the queue with the given queue name
-
+    
     // check if opening the queue was successful
     if (qdes == -1 ) {
         perror("mq_open() failed");
         exit(1);
     }
-
+    
     // open the consumer, producer and values remaining semaphores
     mayConsume = sem_open("/semc_a24joshi", 0);
     mayProduce = sem_open("/semp_a24joshi", 0);
     valuesRemaining = sem_open("/semt_a24joshi", 0);
-
+    
     // consume the items added into the queue by the producer
     consume(atoi(argv[1]));
-
+    
     // try to close the queue
     if (mq_close(qdes) == -1) {
         perror("mq_close() failed");
         exit(2);
     }
-
+    
     // close all the semaphores
     sem_close(mayConsume);
     sem_close(mayProduce);
     sem_close(valuesRemaining);
-
+    
     exit(0);
     return 0;
 }
