4e710495d4511a925ba0c17a89d4ca165089bf6f
Akshay Joshi <a24joshi@uwaterloo.ca>
2014-11-18 17:31:21 -0500

    Remove trailing whitespace.

--- lab5/processes/produce.c
+++ lab5/processes/produce.c
@@ -20,7 +20,7 @@ void produce(int id) {
     // open the semaphores for producer and consumer
     mayConsume = sem_open("/semc_a24joshi", 0);
     mayProduce = sem_open("/semp_a24joshi", 0);
-    
+
     int i;
     // produce items in steps of number of producers
     for (i = id; i < total; i += numProducers) {
@@ -33,7 +33,7 @@ void produce(int id) {
         // increment the consumer semaphore
         sem_post(mayConsume);
     }
-    
+
     // close the two semaphores and exit the producer
     sem_close(mayConsume);
     sem_close(mayProduce);
@@ -51,32 +51,32 @@ int main(int argc, char *argv[]) {
     struct timeval start, complete;
     struct timezone tz;
     gettimeofday(&start, &tz);
-    
+
     char *qname = "/a24joshi_mqueue";
     int oflag = O_RDWR | O_CREAT;
     mode_t mode = S_IRUSR | S_IWUSR;
     struct mq_attr attr;
-    
+
     // check for valid number of arguments
     if ( argc !=5 ) {
         printf("Usage: %s <N> <B> <P> <C>\n", argv[0]);
         exit(1);
     }
-    
+
     // Set the queue attributes
     attr.mq_maxmsg  = atoi(argv[2]);
     attr.mq_msgsize = sizeof(int);
     attr.mq_flags   = 0;            /* a blocking queue  */
-    
+
     // create the queue with the given queue name and attributes
     qdes  = mq_open(qname, oflag, mode, &attr);
-    
+
     // check if creating the queue was successful
     if (qdes == -1 ) {
         perror("mq_open() failed");
         exit(1);
     }
-    
+
     // create the named consumer semaphore
     mayConsume = sem_open("/semc_a24joshi", O_CREAT, mode, 0);
     total = atoi(argv[1]);
@@ -86,11 +86,11 @@ int main(int argc, char *argv[]) {
     valuesRemaining = sem_open("/semt_a24joshi", O_CREAT, mode, total);
     numProducers = atoi(argv[3]);
     numConsumers = atoi(argv[4]);
-    
+
     int producerId = numProducers, consumerId = numConsumers;
     pid_t children[numProducers + numConsumers]; //array of the children pids
-    
-    
+
+
     // if the process is a child, then replace the current process image with a new one
     while (consumerId--) {
         // create a child process using fork
@@ -106,7 +106,7 @@ int main(int argc, char *argv[]) {
             children[consumerId + numProducers] = child_pid;
         }
     }
-    
+
     while (producerId--) {
         // create a child process using fork
         pid_t child_pid = fork();
@@ -117,7 +117,7 @@ int main(int argc, char *argv[]) {
             children[producerId] = child_pid;
         }
     }
-    
+
     // wait for a state change in the child process
     int i;
     for (i = 0; i < numConsumers + numProducers; i++) {
@@ -129,34 +129,34 @@ int main(int argc, char *argv[]) {
             i--;
         }
     }
-    
+
     // try to close the queue
     if (mq_close(qdes) == -1) {
         perror("mq_close() failed");
         exit(2);
     }
-    
+
     // try to remove the queue
     if (mq_unlink(qname) != 0) {
         perror("mq_unlink() failed");
         exit(3);
     }
-    
+
     // close all the semaphores
     sem_close(mayConsume);
     sem_close(mayProduce);
     sem_close(valuesRemaining);
-    
+
     // remove all the semaphores
     sem_unlink("/semp_a24joshi");
     sem_unlink("/semc_a24joshi");
     sem_unlink("/semt_a24joshi");
-    
+
     gettimeofday(&complete, &tz);
-    
+
     // print the initialization and data transmission times
     printf("System execution time: %f seconds\n", computeTime(&complete, &start));
-    
+
     return 0;
 }
 
--- lab5/threads/produce.c
+++ lab5/threads/produce.c
@@ -39,14 +39,14 @@ void* consume(void *id_pointer) {
         pthread_mutex_unlock(&numConsumedMutex);
         pthread_mutex_unlock(&buffer);
         sem_post(&mayProduce);
-        
+
         // do some math to check if item is perfect square
         double root = sqrt(item);
         if (floor(root) == root) {
             printf("%d %d %d\n", id, item, (int)root);
         }
     }
-    
+
     return NULL;
 }
 
@@ -65,7 +65,7 @@ void* produce(void *id_pointer) {
         pthread_mutex_unlock(&buffer);
         sem_post(&mayConsume);
     }
-    
+
     return NULL;
 }
 
@@ -80,19 +80,19 @@ int main(int argc, char *argv[]) {
     struct timeval start, complete;
     struct timezone tz;
     gettimeofday(&start, &tz);
-    
+
     char *qname = "/a24joshi_mqueue";
     int oflag = O_RDWR | O_CREAT;
     mode_t mode = S_IRUSR | S_IWUSR;
     struct mq_attr attr;
-    
+
     // check for valid number of arguments
     if ( argc !=5 ) {
         printf("Usage: %s <N> <B> <P> <C>\n", argv[0]);
         exit(1);
     }
-    
-    
+
+
     total = atoi(argv[1]);
     // initialize the semaphore with number of producers as the initial value
     sem_init(&mayProduce, 0, atoi(argv[2]));
@@ -100,32 +100,32 @@ int main(int argc, char *argv[]) {
     numConsumers = atoi(argv[4]);
     // initialize the semaphore with zero as the initial value
     sem_init(&mayConsume, 0, 0);
-    
+
     // Set the queue attributes
     attr.mq_maxmsg  = atoi(argv[2]);
     attr.mq_msgsize = sizeof(int);
     attr.mq_flags   = 0;            /* a blocking queue  */
-    
+
     // create the queue with the given queue name and attributes
     qdes  = mq_open(qname, oflag, mode, &attr);
-    
+
     // check if creating the queue was successful
     if (qdes == -1 ) {
         perror("mq_open() failed");
         exit(1);
     }
-    
+
     // an array of threads of size equal to total number of producers and consumers
     pthread_t threads[numProducers + numConsumers];
     // arrays of producer and consumer ids
     int producerIds[numProducers];
     int consumerIds[numConsumers];
     int i;
-    
+
     numConsumed = 0;
     pthread_mutex_init(&buffer, NULL);
     pthread_mutex_init(&numConsumedMutex, NULL);
-    
+
     // Create threads for producers, calling produce for every producer while passing the id as a parameter
     for (i = 0; i < numProducers; i++) {
         producerIds[i] = i;
@@ -137,29 +137,29 @@ int main(int argc, char *argv[]) {
         consumerIds[i] = i;
         pthread_create(&threads[numProducers + i], NULL, consume, &consumerIds[i]);
     }
-    
+
     // Wait for all the threads to be done
     for (i = 0; i < numProducers + numConsumers; i++) {
         pthread_join(threads[i], NULL);
     }
-    
+
     // try to close the queue
     if (mq_close(qdes) == -1) {
         perror("mq_close() failed");
         exit(2);
     }
-    
+
     // try to remove the queue
     if (mq_unlink(qname) != 0) {
         perror("mq_unlink() failed");
         exit(3);
     }
-    
+
     gettimeofday(&complete, &tz);
-    
+
     // print the initialization and data transmission times
     printf("System execution time: %f seconds\n", computeTime(&complete, &start));
-    
+
     return 0;
 }
 
